8acf13475c6ed69b54ce791922876e184867e286
manaf_lbis <manaf.lbis@gmail.com>
2025-09-12 09:03:47 +0530

    Testing logs Cleared

--- autofit-backend/src/middlewares/errorHandler.ts
+++ autofit-backend/src/middlewares/errorHandler.ts
@@ -4,9 +4,7 @@ import { HttpStatus } from '../types/responseCode';
 import logger from '../utils/logger';
 
 export const errorHandler = (err: ApiError, req: Request, res: Response, _next:NextFunction ) => {
-
   logger.error(err.message);
-  console.log(err);
   
   const statusCode = err.statusCode || HttpStatus.INTERNAL_SERVER_ERROR;
   const message = err.message || 'Something went wrong';
