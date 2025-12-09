01891d653204456cedfbb0cd8a4c5793e23b6908
manaf_lbis <manaf.lbis@gmail.com>
2025-12-09 08:16:34 +0530

    Files updated

---
+++ autofit-backend/src/types/cloudinary.d.ts
@@ -0,0 +1,9 @@
+interface CloudinaryFile {
+  public_id?: string;
+  url?: string;
+  secure_url?: string;
+  original_filename?: string;
+  filename?: string; 
+  format?: string;
+  bytes?: number;
+}
